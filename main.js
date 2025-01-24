4fa2b532fef331a07d2c737348d29bca4f727063
Olger <olgermercado01@gmail.com>
2025-01-23 19:47:17 -0500

    portafolio

---
+++ main.js
@@ -0,0 +1,75 @@
+new Swiper('.card-wrapper', {
+ 
+    loop: true,
+    spaceBetween: 30,
+  
+    // Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+        0:{
+            slidesPerView: 1
+        },
+        768:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        },
+    }
+  
+  });
+
+
+/*-----------------toggle icon navbar-------------------- */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick= () =>{
+  menuIcon.classList.toggle('fa-xmark');
+  navbar.classList.toggle('active')
+}
+
+/*-----------------croll section-------------------- */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () =>{
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links=>{
+
+
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+      });
+    }
+  });
+  /*-----------------sticky navbar--------------------*/  
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky',window.scrollY>100);
+
+  /*-----------------remove toggle icon and navbar-------------------- */
+  
+  menuIcon.classList.remove('fa-xmark');
+  navbar.classList.remove('active');
+};
+
+  
